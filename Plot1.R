5ebb176e1b35ef3117de3397d161c5d28a0e54a8
Praneet Singavarapu <praneets1@gmail.com>
2016-01-17 01:07:37 +0530

    Assignment1

--- Plot1.R
+++ Plot1.R
@@ -1,3 +1,6 @@
+fileUrl<-"https://archive.ics.uci.edu/ml/machine-learning-databases/00235/household_power_consumption.zip"
+download.file(fileUrl, destfile="household_power_consumption.zip")
+unzip("household_power_consumption.zip", files="household_power_consumption.txt")
 data1 <- read.table("household_power_consumption.txt",header = TRUE, sep=";");
 datasub1 <- subset(data1, as.Date(Date,"%d/%m/%Y")==as.Date("02/01/2007","%m/%d/%Y")|as.Date(Date,"%d/%m/%Y")==as.Date("02/02/2007","%m/%d/%Y"));
 png(file="Plot1.png",width=480,height=480)
--- Plot2.R
+++ Plot2.R
@@ -1,3 +1,6 @@
+fileUrl<-"https://archive.ics.uci.edu/ml/machine-learning-databases/00235/household_power_consumption.zip"
+download.file(fileUrl, destfile="household_power_consumption.zip")
+unzip("household_power_consumption.zip", files="household_power_consumption.txt")
 data1 <- read.table("household_power_consumption.txt",header = TRUE, sep=";");
 datasub1 <- subset(data1, as.Date(Date,"%d/%m/%Y")==as.Date("01/02/2007","%d/%m/%Y")|as.Date(Date,"%d/%m/%Y")==as.Date("02/02/2007","%d/%m/%Y"));
 png(file="Plot2.png",width=480,height=480)
